e64458e1450d65a78778a8a5c9db9f30a6151d3d
JH-Baik78 <92169198+JH-Baik78@users.noreply.github.com>
2022-01-02 22:27:17 +0900

    first commit
    나도 코딩 웹 크롤링 따라해보기

---
+++ 4_정규식.py
@@ -0,0 +1,44 @@
+'''
+정규식을 쓸때는
+1. re.compile('원하는 형태(정규식)')
+2. m = p.match('비교할 문자열') : 주어진 문자열의 처음부터 일치하는지 확인
+3. m = p.search('비교할 문자열') : 주어진 문자열 중에 일치하는게 있는지 확인
+4. lst = p.findall('비교할 문자열') : 일치하는 모든 것을 '리스트' 형태로 반환
+
+원하는 형태 : 정규식
+. (ca.e) : 하나의 문자를 의미 > care, cafe, case.... (o) | caffe(x)
+^ (^de)  : 문자열의 시작      > desk, destination (o) | fade (x)
+$ (se$)  : 문자열의 끝        > case, base (o) | face (x)
+'''
+
+import re
+
+# p = re.compile('ca.e')  
+# if m:
+#   print(m.group())
+# else:
+#   print("매칭되지 않음")
+
+
+# 함수로 만들어 보자.
+def print_match(word):
+  m = p.match(word)
+  if m:
+    print('m.group():', m.group())   # 일치하는 문자열 반환
+    print('m.string:', m.string)     # 입력받은 문자열
+    print('m.start():', m.start())   # 일치하는 문자열의 시작 index
+    print('m.end()', m.end())        # 일치하는 문자열의 끝 index
+    print('m.span()', m.span())      # 일치하는 문자열의 시작 / 끝 index
+  else:
+    print("매칭되지 않음")
+    
+# print_match('careless')
+
+# m = p.match('caffe')               # 주어진 문자열의 처음부터 일치하는지 확인
+# print(m.group())                   # 매치되지 않으면 에러 발생
+# m = p.search('caffe')              # 주어진 문자열 중에 일치하는게 있는지 확인
+
+m = p.findall('careless')            # findall : 일치하는 모든 것을 리스트 형태로 반환
+lst = p.findall('good care cafe')
+print(lst)
+
